f4bd1298a7f68691e3e91f3657bfab90bbb12298
keltam27 <k736812950@gmail.com>
2023-04-02 06:06:01 +0800

    update

--- script.js
+++ script.js
@@ -45,6 +45,12 @@ const app = new Vue({
     welcome_description: 'Discover limitless possibilities through immersive experiences in the metaverse -\n where imagination meets reality, and technology connects humanity.',
     question: "What is Metaverse?",
     answer: "Welcome to the metaverse, a virtual world where imagination meets reality. This digital realm allows users to escape everyday life and explore new horizons through immersive experiences. From gaming to socializing, the metaverse offers endless opportunities to connect with others and discover new realms of possibility. With cutting-edge technology and limitless creativity, the metaverse opens a new chapter in the evolution of human interaction. Join us on this exciting journey into the unknown and discover the endless possibilities of the metaverse.",
+    box1_title: "Virtual Concerts",
+    box1_description: "Experience concerts like never before, attend from anywhere in the world and see your favorite artists perform in the metaverse.",
+    box2_title: "Virtual Reality Training",
+    box2_description: "Use VR technology to train employees or students in immersive environments that simulate real-life situations.",
+    box3_title: "Virtual Real Estate",
+    box3_description: "Buy, sell, rent or explore properties in the metaverse. Design your dream house or office and experience it virtually.",
     activeIndex: 0,
     currentIndex: 0,
     text: {
@@ -53,22 +59,40 @@ const app = new Vue({
         "title_description": "Examine the possibilities of what the metaverse may hold ahead.",
         "welcome": "Welcome to Metaverse",
         "welcome_description": "Discover limitless possibilities through immersive experiences in the metaverse -\n where imagination meets reality, and technology connects humanity.",
-        "question": "What is Metaverse?",
-        "answer": "A virtual world where imagination meets reality. This digital realm allows users to escape everyday life and explore new horizons through immersive experiences. From gaming to socializing, the metaverse offers endless opportunities to connect with others and discover new realms of possibility. With cutting-edge technology and limitless creativity, the metaverse opens a new chapter in the evolution of human interaction. Join us on this exciting journey into the unknown and discover the endless possibilities of the metaverse."
+        "question": "Work in the metaverse",
+        "answer": "In the metaverse, work becomes seamless and limitless. Collaborating with colleagues from around the world is now a reality with virtual workplaces, enabling productivity without the barriers of distance. With innovative tools and interfacing technologies, work in the metaverse is an experience of seamless immersive, interactive environments. Join us in shaping the future of work, where creativity thrives and boundaries do not exist.",
+        "box1_title": "Virtual Concerts",
+        "box1_description": "Experience concerts like never before, attend from anywhere in the world and see your favorite artists perform in the metaverse.",
+        "box2_title": "Virtual Reality Training",
+        "box2_description": "Use VR technology to train employees or students in immersive environments that simulate real-life situations.",
+        "box3_title": "Virtual Real Estate",
+        "box3_description": "Buy, sell, rent or explore properties in the metaverse. Design your dream house or office and experience it virtually.",
       }, "zh-mo": {
         "title": "相見元宇宙",
         "title_description": "探索元宇宙\n未來或許會帶來的\n無限可能性。",
         "welcome": "歡迎來到元宇宙",
         "welcome_description": "在元宇宙中，透過身臨其境的體驗，探索無限可能，讓想像力與現實交會，科技串聯人類。",
-        "question": "甚麼是元宇宙？",
-        "answer": "一個虛擬世界，想像與現實交錯。這個數位領域讓使用者從日常生活中解放，探索更寬廣的世界。從遊戲到社交，元宇宙提供無限機會與他人連結，發現新的可能性。使用先進的技術和無限的創意，元宇宙也為人類互動的演進增添了新的一頁。一起加入這場闖蕩未知領域的旅程，發掘無限可能性吧。"
+        "question": "在元宇宙工作",
+        "answer": "在元宇宙中，工作變得無限且無縫。有了虛擬工作場所，讓您與世界各地的同事合作成為現實，不再受距離的阻礙，提高生產力。利用先進的工具和介面技術，元宇宙中的工作環境變得更加真實、沉浸和互動。加入我們，塑造未來的工作場景，讓創造力蓬勃發展，打破所有界限。",
+        "box1_title": "虛擬演唱會",
+        "box1_description": "以前所未有的方式感受演唱會。在元宇宙中，不論身在何處，都能與您喜愛的演唱家共度美好時光。",
+        "box2_title": "虛擬現實培訓",
+        "box2_description": "使用 VR 技術，以沉浸式的環境訓練員工或學生，提升實戰能力。",
+        "box3_title": "虛擬房地產",
+        "box3_description": "在元宇宙中買賣、租賃或探索房產。打造夢想住宅或辦公室，體驗逼真的虛擬房地產。",
       }, "zh-cn": {
         "title": "相見元宇宙",
         "title_description": "探索元宇宙\n未来或许会带来的\n无限可能性。",
         "welcome": "欢迎来到元宇宙",
         "welcome_description": "在元宇宙中，通过身临其境的体验，探索无限可能，让想像力与现实交会，科技串联人类。",
-        "question": "什么是元宇宙？",
-        "answer": "一个虚拟世界，想象与现实交错。这个数字领域让使用者从日常生活中解放，探索更宽广的世界。从游戏到社交，元宇宙提供无限机会与他人连结，发现新的可能性。使用先进的技术和无限的创意，元宇宙也为人类互动的演变增添了新的一页。一起加入这场闯荡未知领域的旅程，发掘无限可能性吧。"
+        "question": "在元宇宙工作",
+        "answer": "在元宇宙中，工作变得无限且无缝。有了虚拟工作场所，让您与世界各地的同事合作成为现实，不再受距离的阻碍，提高生产率。利用先进的工具和界面技术，元宇宙中的工作环境变得更加真实、沉浸和互动。加入我们，塑造未来的工作场景，让创造力蓬勃发展，打破所有界限。",
+        "box1_title": "虚拟演唱会",
+        "box1_description": "以前所未有的方式感受演唱会。在元宇宙中，不论身在何处，都能与您喜爱的演唱家共度美好时光。",
+        "box2_title": "虚拟现实培训",
+        "box2_description": "使用 VR 技术，以沉浸式的环境训练员工或学生，提升实战能力。",
+        "box3_title": "虚拟房地产",
+        "box3_description": "在元宇宙中买卖、租赁或探索房产。打造梦想住宅或办公室，体验逼真的虚拟房地产。",
       }
     }
   },
@@ -85,6 +109,12 @@ const app = new Vue({
       this.welcome_description = this.text[lang].welcome_description;
       this.question = this.text[lang].question;
       this.answer = this.text[lang].answer;
+      this.box1_title = this.text[lang].box1_title;
+      this.box1_description = this.text[lang].box1_description;
+      this.box2_title = this.text[lang].box2_title;
+      this.box2_description = this.text[lang].box2_description;
+      this.box3_title = this.text[lang].box3_title;
+      this.box3_description = this.text[lang].box3_description;
       // Fetch the quotes for the current language
       this.loading = true;
       fetch(`https://backend.cpsumsu.org/api/metaverse_quotes`)
